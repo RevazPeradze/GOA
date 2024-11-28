86e5aef0544775c8ab5499f889fee99eb1c35566
RevazPeradze <my.reziko@gmail.com>
2024-11-28 21:34:00 +0400

    Delete lvl15 directory

--- lvl15/classwork/classwork.py
+++
@@ -1,6 +0,0 @@
-temperature = 32 
-
-if temperature > 30:
-    print("It's hot outside!")
-else:
-    print("It's not too hot.")
--- lvl15/homework/homework1.py
+++
@@ -1,8 +0,0 @@
-def check_voting_eligibility(age):
-    if age >= 18:
-        print("You are eligible to vote.")
-    else:
-        print("You are not eligible to vote yet.")
-
-age = int(input("Enter your age: "))
-check_voting_eligibility(age)
